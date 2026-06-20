df0e283b6ecd7ac89726c287d966222a89cf0d94
zawatton <kurozawawo@gmail.com>
2026-06-20 10:37:01 +0900

    Extract bridge emoji writeback chunk

--- test/nemacs-gui-file-bridge-runtime-test.el
+++ test/nemacs-gui-file-bridge-runtime-test.el
@@ -16323,6 +16323,91 @@ report real Git state, diff, and log (M2 Project/Git close-gate)."
         (when (file-exists-p probe-file)
           (delete-file probe-file))))))
 
+(ert-deftest nemacs-gui-file-bridge-runtime-test/standalone-bridge-emoji-writeback-helper ()
+  "Bridge emoji writeback helper should write matching state."
+  (nemacs-gui-file-bridge-runtime-test--skip-unless-reader
+    (let ((reader (nemacs-gui-file-bridge-runtime-test--reader))
+          (image (nemacs-gui-file-bridge-runtime-test--write-image))
+          (probe-file "/tmp/nemacs-bridge-emoji-writeback-helper"))
+      (unwind-protect
+          (nemacs-gui-file-bridge-runtime-test--with-transport
+            (let ((result
+                   (nemacs-gui-file-bridge-runtime-test--run-ok
+                    reader image
+                    "(progn
+                       (setq files--buffer-string \"emoji buffer\")
+                       (setq files--current-file-name \"/tmp/emoji.org\")
+                       (setq files--buffer-name \"*Emoji*\")
+                       (setq files--buffer-read-only-p t)
+                       (setq files--modeline-string \"Emoji zoom 1\")
+                       (setq files--point 8)
+                       (setq files--mark 3)
+                       (setq files--window-start 2)
+                       (fset 'capture-emoji-writeback
+                             (lambda (command)
+                               (setq files--bridge-status \"ok\")
+                               (let ((returned
+                                      (files--bridge-emoji-writeback-current-context
+                                       command)))
+                                 (concat returned
+                                         \":\"
+                                         files--bridge-status))))
+                       (nl-write-file
+                        \"/tmp/nemacs-bridge-emoji-writeback-helper\"
+                        (concat
+                         (capture-emoji-writeback \"emoji-insert\")
+                         \"\\t\"
+                         (capture-emoji-writeback \"emoji-list\")
+                         \"\\t\"
+                         (capture-emoji-writeback \"emoji-recent\")
+                         \"\\t\"
+                         (capture-emoji-writeback \"emoji-search\")
+                         \"\\t\"
+                         (capture-emoji-writeback \"emoji-describe\")
+                         \"\\t\"
+                         (capture-emoji-writeback
+                          \"emoji-zoom-increase\")
+                         \"\\t\"
+                         (capture-emoji-writeback
+                          \"emoji-zoom-decrease\")
+                         \"\\t\"
+                         (capture-emoji-writeback \"emoji-zoom-reset\")
+                         \"\\t\"
+                         (capture-emoji-writeback
+                          \"forward-char\"))))")))
+              (should (equal 0 (plist-get result :status)))
+              (should (equal "emoji-insert:written\temoji-list:written\temoji-recent:written\temoji-search:written\temoji-describe:written\temoji-zoom-increase:written\temoji-zoom-decrease:written\temoji-zoom-reset:written\tforward-char:ok"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              probe-file)))
+              (should (equal "emoji buffer"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-buf")))
+              (should (equal "/tmp/emoji.org"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-file")))
+              (should (equal "*Emoji*"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-buffer-name")))
+              (should (equal "1"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-read-only")))
+              (should (equal "Emoji zoom 1"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-modeline")))
+              (should (equal "00008"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-point")))
+              (should (equal "00003"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-mark")))
+              (should (equal "00002"
+                             (nemacs-gui-file-bridge-runtime-test--slurp
+                              "/tmp/nemacs-window-start")))))
+        (when (file-exists-p image)
+          (delete-file image))
+        (when (file-exists-p probe-file)
+          (delete-file probe-file))))))
+
 (provide 'nemacs-gui-file-bridge-runtime-test)
 
 ;;; nemacs-gui-file-bridge-runtime-test.el ends here
